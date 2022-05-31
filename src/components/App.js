5ae657f563e7a3600369568e34ccfdc7dc6c21fa
Rushan Shakirzianov <rushan.shakirzianov@gmail.com>
2022-05-31 23:51:16 +0300

    feat: create app via react, add some rules

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+//import logo from '../logo.svg';
+import React from "react";
+import Header from "./Header";
+import AppContainer from "./AppContainer";
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <AppContainer />
+    </>
+  );
+}
+
+export default App;
